2b4f2b1ccc2eceac76e6514ca6acff8025323359
david <ypchou.david@gmail.com>
2022-08-25 13:12:01 -0700

    Add an API router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,22 @@
+// backend/routes/api/index.js
+const router = require('express').Router();
+
+
+
+// backend/routes/api/index.js
+// ...
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+  // ...
+
+
+
+
+
+
+
+
+module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -24,12 +24,15 @@ router.get("/api/csrf/restore", (req, res) => {
 
 
 
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
+// ...
 
 
 
 
 
 module.exports = router;
-
-//Phase0 done
