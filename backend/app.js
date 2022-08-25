61653adeaf28995a1f1b6706965f974f2db9f2b5
david <ypchou.david@gmail.com>
2022-08-25 14:10:07 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -45,17 +45,66 @@ app.use(
     })
 );
 
+
+
 //Add the routes to the Express application by importing with the other imports in backend/app.js and connecting the exported router to app after all the middlewares.
 // backend/app.js
 const routes = require('./routes');
 
-// ...
+
 
 app.use(routes); // Connect all the routes
+
+
+// VVV ERROR HANDLERS VVV
+
+//The first error handler is actually just a regular middleware. It will catch any requests that don't match any of the routes defined and create a server error with a status code of 404
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+
+//The second error handler is for catching Sequelize errors and formatting them before sending the error response.
+const { ValidationError } = require('sequelize');
+
+// ...
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+
+// The last error handler is for formatting all the errors before returning a JSON response. It will include the error message, the errors array, and the error stack trace (if the environment is in development) with the status code of the error message.
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
+
+
+
+
+
 
 
 //export app
 // backend/app.js
 // ...
-
 module.exports = app;
