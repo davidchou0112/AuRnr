374e6a935f7b8be511922e5a7f1954a87dcaf6c0
david <ypchou.david@gmail.com>
2022-08-30 14:59:43 -0700

    testing

--- backend/db/seeders/20220830204608-demo-spots.js
+++ backend/db/seeders/20220830204608-demo-spots.js
@@ -22,31 +22,31 @@ module.exports = {
         lng: -122.4730327,
         name: 'App Academy',
         description: 'Place where web developers are created',
-        price: 123
+        price: 69
       },
       {
         ownerId: 2,
-        address: '123 Disney Lane',
-        city: '',
-        state: '',
-        country: '',
-        lat: ,
-        lng: ,
-        name: '',
-        description: '',
-        price: ''
+        address: '149 Diamond Street',
+        city: 'Arcadia',
+        state: 'California',
+        country: 'United States of America',
+        lat: 35.25,
+        lng: 14.42,
+        name: 'House of Diamonds',
+        description: 'Bling Bling Bling',
+        price: 99999
       },
       {
         ownerId: 3,
-        address: '123 Disney Lane',
-        city: '',
-        state: '',
-        country: '',
-        lat: ,
-        lng: ,
-        name: '',
-        description: '',
-        price: ''
+        address: '455 Union St',
+        city: 'Arcata',
+        state: 'California',
+        country: 'United States of America',
+        lat: 38.54,
+        lng: 48.35,
+        name: 'Humboldt Grown',
+        description: 'Grass is greener on this side',
+        price: 420
       }
     ])
   },
