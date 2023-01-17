28913ebb5613358a3f7860ad26221bde4a0bc409
david <ypchou.david@gmail.com>
2023-01-17 11:46:14 -0800

    testing

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -56,22 +56,14 @@ export const clearSpot = () => {
 
 
 export const getAllReviews = (spotId) => async (dispatch) => {
-    // console.log('is this getting here?')
-    // console.log(`~~~~~~~~~~~~spotId~~~`, spotId)
-
     const response = await fetch(`/api/spots/${spotId}/reviews`);
-    // console.log(`~~~~~~~~~~~~response~~~`, response)
 
     if (response.ok) {
-
         const reviews = await response.json();
         dispatch(allReviews(reviews));
-        // console.log(`~~~~~~~~~~~~reviews~~~`, reviews)
-        // console.log(`~~~~~~~~~~~~response~~~`, response)
-
         return reviews;
     }
-    // return null;
+
 }
 
 export const getMyReviews = () => async (dispatch) => {
@@ -81,14 +73,10 @@ export const getMyReviews = () => async (dispatch) => {
         dispatch(allReviews(reviews));
         return reviews;
     }
-    // return null;
 }
 
-export const createOneReview = (review, spotId, url) => async (dispatch) => {
-    // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~url:', url)
-    // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~review:', review)
-    // console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~spotId:', spotId)
-    // review.stars = review.ratingNum;
+export const createOneReview = (review, spotId) => async (dispatch) => {
+
     const response = await csrfFetch(`/api/spots/${spotId}/reviews`, {
         method: 'post',
         headers: {
@@ -118,39 +106,6 @@ export const createOneReview = (review, spotId, url) => async (dispatch) => {
     // console.log('~~~~~~~~~~~~~~~~~~~~createOneReview Thunk - newReview:', newReview)
     dispatch(createReview(newReview));
 
-    // const imgRes = await csrfFetch(`/api/reviews/${newReview.id}/images`, {
-    //     method: 'post',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({ url })
-    // })
-    // console.log('~~~~~~~~~~~~~~~~~~~~createOneReview Thunk - imgRes:', imgRes)
-
-
-    // if (!imgRes.ok) {
-    //     let imgError;
-    //     if (imgRes.status === 404) {
-    //         imgError = await imgRes.json();
-    //         // throw new ValidationError(error.errors, response.statusText);
-    //         return imgError;
-    //     } else {
-    //         let imgErrorJSON;
-    //         imgError = await imgRes.text();
-    //         try {
-    //             imgErrorJSON = JSON.parse(imgError);
-    //         } catch {
-    //             throw new Error(imgError);
-    //         }
-    //         throw new Error(`${imgErrorJSON.title}: ${imgErrorJSON.message}`)
-    //     }
-    // }
-
-    // const newImg = await imgRes.json()
-    // dispatch(addReviewImg(newReview.id, url))
-    // console.log('createOneReview Thunk - newImg:', newImg)
-    // newReview['ReviewImages'] = newImg;
-    // console.log('createOneReview Thunk - newReview:', newReview)
     return newReview;
 }
 
